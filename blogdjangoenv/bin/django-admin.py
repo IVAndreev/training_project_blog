70eaa74d524d290578a301420374f853d361ca74
IVAndreev <andreewxiii@yandex.ru>
2019-09-05 16:59:19 +0300

    Изменен HTML на сайте.

---
+++ blogdjangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aiv/Документы/prog/training_project_blog/blogdjangoenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
